ec45b4748f7b1c03cdadeb782fc5cc7d3bcbb56d
Alan Wen <36051502+wenjalan@users.noreply.github.com>
2021-05-11 23:18:01 -0700

    Basic Gallery structure

--- app/src/App.js
+++ app/src/App.js
@@ -1,22 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
 
+function Gallery() {
+  return (
+    <div className="Gallery">
+      <div className="Gallery-row">
+        <div className="Gallery-item">Item</div>
+        <div className="Gallery-item">Item</div>
+        <div className="Gallery-item">Item</div>
+      </div>
+      <div className="Gallery-row">
+        <div className="Gallery-item">Item</div>
+        <div className="Gallery-item">Item</div>
+      </div>
+      <div className="Gallery-row">
+        <div className="Gallery-item">Item</div>
+        <div className="Gallery-item">Item</div>
+        <div className="Gallery-item">Item</div>
+        <div className="Gallery-item">Item</div>
+      </div>
+    </div>
+  )
+}
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>#channel-name</h1>
+        <div className="Divider"/>
+        <Gallery/>
       </header>
     </div>
   );
